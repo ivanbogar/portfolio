de8e9c01c6cb2289f0a208cb9392c40086877b09
ivanbogar <ivanbogar@gmail.com>
2021-02-06 15:42:16 -0300

    Landing-random-images

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  images: string[] = ['../../assets/images/avatar.png', '../../assets/images/avatar2.png','../../assets/images/avatar3.png','../../assets/images/avatar4.png', '../../assets/images/avatar5.png'];
+
+  backgroundImage: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    let ran = Math.round((Math.random()*100)%5);
+    console.log(ran, Math.random()*100)
+    this.backgroundImage = this.images[ran];
   }
 
 }
+
+
+
